dfc33e40cf70a07f346e66f130158700b110ddab
tiketdatahaggy <muhammad.haggy@tiket.com>
2025-07-08 20:48:31 +0700

    remove err

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import { Plus, Home, Search, MessageSquare, Crown } from 'lucide-react';
+import { Home, Search, MessageSquare } from 'lucide-react';
 import LoginButton from './LoginButton';
 import { useSearch } from '@/app/_contexts/SearchContext';
 
